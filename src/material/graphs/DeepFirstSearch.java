4af22edef3f90bc6d2b0b5a298d4dada23a02555
amart <amart@192.168.1.43>
2016-12-15 18:51:08 +0100

    buesqueda en profundidad

--- src/material/graphs/DeepFirstSearch.java
+++ src/material/graphs/DeepFirstSearch.java
@@ -1,6 +1,9 @@
 package material.graphs;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Stack;
 
 /**
  *
@@ -17,6 +20,51 @@ public class DeepFirstSearch <V,E> {
      * @return the list of edges from v1 to v2
      */
     public List <Edge <E>> getPath(Graph <V,E> g, Vertex <V> v1, Vertex <V> v2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(g.vertices().contains(v1)&& g.vertices().contains(v2)){
+            
+            Stack<Edge> pila = new Stack<>();
+            List<Edge<E>> listVisited = new ArrayList<>();
+            Collection listAdj = g.incidentEdges(v1);
+           
+            for(Object elem: listAdj){
+                pila.add((Edge) elem);
+            }
+            
+            while(!pila.empty()){
+                //ELEdge vaux =checkEdge(pila.pop()) ;
+                Edge vaux = pila.pop();
+                if(vaux!=null /*&& !listVisited.contains(vaux)*/){
+                    listVisited.add(vaux);
+
+                    if(vaux.equals(v2)/*vaux.getEndVertex().equals(v2)*/){
+                        return listVisited;
+                    }
+                    else{
+                        listAdj = g.incidentEdges((Vertex<V>) vaux);
+                        for(Object elem: listAdj)
+                            pila.add((Edge) elem);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    private ELEdge<E,V> checkEdge(Edge<E> edge) {
+        if (edge instanceof ELEdge){
+            ELEdge<E, V> e = (ELEdge<E,V>)edge;
+            if (e.getGraph() == this)
+                return e;
+        }
+        throw new RuntimeException("The edge is not in the graph");
+    }
+
+    private ELVertex<V> checkVertex(Vertex<V> vertex) {
+        if (vertex instanceof ELVertex){
+            ELVertex<V> v = (ELVertex<V>)vertex;
+            if (v.getGraph() == this)
+                return v;
+        }
+        throw new RuntimeException("The vertex is not in the graph");        
     }
 }
